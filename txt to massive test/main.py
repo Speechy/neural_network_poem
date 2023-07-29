d612f45573df92dfd9968a0ecc1db4b05116f0a1
DanilKravtsov <95885262+WhiteLineST@users.noreply.github.com>
2023-07-29 13:53:19 +0300

    29.07.23

--- txt to massive test/main.py
+++ txt to massive test/main.py
@@ -1,8 +1,22 @@
-file = open('../test.txt', encoding='utf-8')
+import string
+file1 = open('../test2.txt', encoding='utf-8')
 # print(file.read()) # вывести весь текст
 
-with open('../test.txt', encoding='utf-8') as fp: #Подсчёт количества строк
+with open('../test2.txt', encoding='utf-8') as fp: #Подсчёт количества строк
     lines = len(fp.readlines())
     print('Total Number of lines:', lines)
-arr =file.read().split()    #текст в массив слов
+
+arr = file1.read().split(" ")   #текст в массив слов     скорее всего бесполезен для нас
+print(arr)
+
+#--------------------------
+
+file2 = open('../test3.txt', encoding='utf-8')
+# print(file.read()) # вывести весь текст
+
+with open('../test3.txt', encoding='utf-8') as fp: #Подсчёт количества строк
+    lines = len(fp.readlines())
+    print('Total Number of lines:', lines)
+
+arr = file2.read().split(".")   #текст в массив строк
 print(arr)
